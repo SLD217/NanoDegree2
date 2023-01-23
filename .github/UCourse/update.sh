b7220c7979ab149bb8c9102bfcbcd03202f0c128
SLD217 <neal.mcconnell@gmail.com>
2023-01-23 08:26:39 +0000

    Update from the weekend

---
+++ .github/UCourse/update.sh
@@ -0,0 +1,1 @@
+aws cloudformation update-stack --stack-name $1 --template-body file://.github\UCourse\$2  --parameters file://.github\UCourse\$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
